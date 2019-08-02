6f1f0451db58a02098e0fc6d1b72a002655718cd
User2 <User2@DESKTOP-J1T0NLC>
2019-08-02 13:55:23 +0100

    Initial Commit

---
+++ DependencyCheck/src/main/java/com/alan/main/DepCheckMain.java
@@ -0,0 +1,10 @@
+package com.alan.main;
+
+public class DepCheckMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DependencyCheck/src/main/java/com/alan/main/Play.java
@@ -0,0 +1,5 @@
+package com.alan.main;
+
+public class Play {
+
+}
